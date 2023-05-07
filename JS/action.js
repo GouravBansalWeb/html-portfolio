8b9dad49ad10d3f4475dc50a1b75477c1694a9d8
Gourav Bansal <71654386+GouravBansalWeb@users.noreply.github.com>
2023-05-07 12:21:01 +0530

    Update action.js

--- JS/action.js
+++ JS/action.js
@@ -1,1 +1,36 @@
 alert("Welcome to Gourav's Portfolio");
+
+
+
+function date()
+{
+var d=new Date();
+var h=d.getHours();
+var m=d.getMinutes();
+var s=d.getSeconds();
+var dat=d.getDate();
+var mth=d.getMonth();
+var yr=d.getFullYear();
+var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var dy=days[d.getDay()];
+var session="Am";
+if (dat<=12)
+{
+    session="Pm";
+}
+if(h>12)
+{
+h=h-12;
+}
+var hour=(h<10) ? "0"+h:h;
+var minute=(m<10)?"0"+m:m;
+var second=(s<10)?"0"+s:s;
+var dat=(dat<10)?"0"+dat:dat;
+var mth=(mth<10)?"0"+mth:mth;
+
+
+document.getElementsByTagName("h5")[0].innerText="Date "+" "+":-" +" "+dat+"/"+mth+"/"+yr;
+document.getElementsByTagName("h5")[1].innerText="Time "+" "+":-" +" "+hour+":"+minute+":"+second+" "+session+" ";
+document.getElementsByTagName("h5")[2].innerText=" Day"+" "+":-"+" "+dy ;
+    setTimeout(date,1000);
+}
